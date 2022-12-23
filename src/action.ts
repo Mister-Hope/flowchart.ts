c4f3aaf83559b19fcc4245bc04f57cebbc3cd730
Mr.Hope <mister-hope@outlook.com>
2022-12-23 20:00:06 +0800

    chore: tweaks

--- src/action.ts
+++ src/action.ts
@@ -3,6 +3,66 @@ import { FlowChart } from "./chart";
 import type { RaphaelPath, RaphaelTextAnchorType } from "raphael";
 import type { Position } from "./symbol/symbol";
 
+export interface LineIntersectionResult {
+  x: number | null;
+  y: number | null;
+  onLine1: boolean;
+  onLine2: boolean;
+}
+
+export const checkLineIntersection = (
+  line1StartX: number,
+  line1StartY: number,
+  line1EndX: number,
+  line1EndY: number,
+  line2StartX: number,
+  line2StartY: number,
+  line2EndX: number,
+  line2EndY: number
+): LineIntersectionResult => {
+  // if the lines intersect, the result contains the x and y of the intersection (treating the lines as infinite) and booleans for whether line segment 1 or line segment 2 contain the point
+  const result: LineIntersectionResult = {
+    x: null,
+    y: null,
+    onLine1: false,
+    onLine2: false,
+  };
+  const denominator =
+    (line2EndY - line2StartY) * (line1EndX - line1StartX) -
+    (line2EndX - line2StartX) * (line1EndY - line1StartY);
+
+  if (denominator === 0) return result;
+
+  const yDistance = line1StartY - line2StartY;
+  const xDistance = line1StartX - line2StartX;
+  const numerator1 =
+    (line2EndX - line2StartX) * yDistance -
+    (line2EndY - line2StartY) * xDistance;
+  const numerator2 =
+    (line1EndX - line1StartX) * yDistance -
+    (line1EndY - line1StartY) * xDistance;
+
+  const a = numerator1 / denominator;
+  const b = numerator2 / denominator;
+
+  // if we cast these lines infinitely in both directions, they intersect here:
+  result.x = line1StartX + a * (line1EndX - line1StartX);
+  result.y = line1StartY + a * (line1EndY - line1StartY);
+  /*
+  // it is worth noting that this should be the same as:
+  x = line2StartX + (b * (line2EndX - line2StartX));
+  y = line2StartX + (b * (line2EndY - line2StartY));
+  */
+  // if line1 is a segment and line2 is infinite, they intersect if:
+  if (a > 0 && a < 1) result.onLine1 = true;
+
+  // if line2 is a segment and line1 is infinite, they intersect if:
+  if (b > 0 && b < 1) result.onLine2 = true;
+
+  // if line1 and line2 are segments, they intersect if both of the above are true
+  return result;
+};
+
 export const drawPath = (
   chart: FlowChart,
   location: Position,
@@ -124,64 +184,4 @@ export const drawLine = (
   }
 
   return line;
-};
-
-export interface LineIntersectionResult {
-  x: number | null;
-  y: number | null;
-  onLine1: boolean;
-  onLine2: boolean;
-}
-
-export const checkLineIntersection = (
-  line1StartX: number,
-  line1StartY: number,
-  line1EndX: number,
-  line1EndY: number,
-  line2StartX: number,
-  line2StartY: number,
-  line2EndX: number,
-  line2EndY: number
-): LineIntersectionResult => {
-  // if the lines intersect, the result contains the x and y of the intersection (treating the lines as infinite) and booleans for whether line segment 1 or line segment 2 contain the point
-  const result: LineIntersectionResult = {
-    x: null,
-    y: null,
-    onLine1: false,
-    onLine2: false,
-  };
-  const denominator =
-    (line2EndY - line2StartY) * (line1EndX - line1StartX) -
-    (line2EndX - line2StartX) * (line1EndY - line1StartY);
-
-  if (denominator === 0) return result;
-
-  const yDistance = line1StartY - line2StartY;
-  const xDistance = line1StartX - line2StartX;
-  const numerator1 =
-    (line2EndX - line2StartX) * yDistance -
-    (line2EndY - line2StartY) * xDistance;
-  const numerator2 =
-    (line1EndX - line1StartX) * yDistance -
-    (line1EndY - line1StartY) * xDistance;
-
-  const a = numerator1 / denominator;
-  const b = numerator2 / denominator;
-
-  // if we cast these lines infinitely in both directions, they intersect here:
-  result.x = line1StartX + a * (line1EndX - line1StartX);
-  result.y = line1StartY + a * (line1EndY - line1StartY);
-  /*
-  // it is worth noting that this should be the same as:
-  x = line2StartX + (b * (line2EndX - line2StartX));
-  y = line2StartX + (b * (line2EndY - line2StartY));
-  */
-  // if line1 is a segment and line2 is infinite, they intersect if:
-  if (a > 0 && a < 1) result.onLine1 = true;
-
-  // if line2 is a segment and line1 is infinite, they intersect if:
-  if (b > 0 && b < 1) result.onLine2 = true;
-
-  // if line1 and line2 are segments, they intersect if both of the above are true
-  return result;
 };
